242f19741266ace0edb710d3a66dfcda99393572
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-12-19 17:13:14 +0000

    Propriedades

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import { Post } from './Post'
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Marcus Vinícius"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius nemo beatae enim tempora odio ex eligendi, magni quas officiis iusto commodi aperiam, nulla ea exercitationem sit repudiandae totam corrupti?"
+      />
+
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
     </>
-  );
+  )
 }
