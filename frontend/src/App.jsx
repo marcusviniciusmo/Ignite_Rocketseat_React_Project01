e6f5f8c3ffc6d0a19fd02d3fbd8e7def9e6985e0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-12-21 14:18:57 +0000

    Componente: Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,28 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
-import './global.css';
+import styles from './App.module.css';
+import './global.css'
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post
-        author="Marcus Vinícius"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius nemo beatae enim tempora odio ex eligendi, magni quas officiis iusto commodi aperiam, nulla ea exercitationem sit repudiandae totam corrupti?"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Marcus Vinícius"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius nemo beatae enim tempora odio ex eligendi, magni quas officiis iusto commodi aperiam, nulla ea exercitationem sit repudiandae totam corrupti?"
+          />
 
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </>
   )
 }
