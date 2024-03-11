15af836afda5649b96272d296de265a9c0a7ab68
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2024-03-11 22:21:45 +0000

    Comunicação entre componentes

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { Avatar } from './Avatar';
 import { Trash, ThumbsUp } from '@phosphor-icons/react';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  };
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
